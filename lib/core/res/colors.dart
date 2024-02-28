673b01c95d9b674a26bf4eeb5cd12d87bfb5bbba
Yihun <yankure01@gmail.com>
2024-02-28 16:08:32 +0300

    fdb-01

---
+++ lib/core/res/colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Colours {
+
+  static const gradient = [
+    Color(0xFFEDF8FF),
+    Color(0xFFFDC1E8),
+    Color(0xFFFFFFFF),
+    Color(0xFFFDFEA1),
+  ];
+
+  static const primaryColour =  Color(0xFF458CFF);
+
+  static const neutralColour = Color(0xFF757CBE);
+
+  static const physicsTileColour = Color(0xFFD3D5FE);
+
+  static const redColour = Color(0xFFFF5C5C);
+
+  static const greenColour = Color(0xFF28CA6C);
+
+  static const chatFieldColour = Color(0xFFF4F5F6);
+
+  static const chatFieldColourDarker = Color(0xffe8e9ea);
+
+  static const currentUserChatBubbleColour = Color(0xFF2196F3);
+
+  static const otherUserChatBubbleColour = Color(0xFFEEEEEE);
+
+  static const currentUserChatBubbleColourDarker = Color(0xFF1976D2);
+
+  static const otherUserChatBubbleColourDarker = Color(0xFFE0E0E0);
+
+  const Colours._();
+}
