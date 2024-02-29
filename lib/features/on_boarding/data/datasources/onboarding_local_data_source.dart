8db7ac167444d8b5efe6399061fd9c1612d28ec2
Yihun <yankure01@gmail.com>
2024-02-29 17:22:31 +0300

    onboarding local_datasource

--- lib/features/on_boarding/data/datasources/onboarding_local_data_source.dart
+++ lib/features/on_boarding/data/datasources/onboarding_local_data_source.dart
@@ -1,21 +1,34 @@
+import 'package:online_learning/core/errors/exception.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class OnBoardingLocalDataSource {
   Future<void> cacheFirstTimer();
   Future<bool> checkIfUserFirstTimer();
 }
 
+const kFirstTimerKey = 'first_timer';
+
 class OnBoardingLocalDataSourceImpl implements OnBoardingLocalDataSource {
+  final SharedPreferences _prefs;
+
+  OnBoardingLocalDataSourceImpl({required SharedPreferences prefs})
+      : _prefs = prefs;
 
   @override
-  Future<void> cacheFirstTimer() {
-    // TODO: implement cacheFirstTimer
-    throw UnimplementedError();
+  Future<void> cacheFirstTimer() async {
+    try {
+      await _prefs.setBool(kFirstTimerKey, false);
+    } catch (e) {
+      throw CacheException(message: e.toString());
+    }
   }
 
   @override
-  Future<bool> checkIfUserFirstTimer() {
-    // TODO: implement checkIfUserFirstTimer
-    throw UnimplementedError();
+  Future<bool> checkIfUserFirstTimer() async{
+    try {
+      return _prefs.getBool(kFirstTimerKey) ?? true;
+    } catch (e) {
+      throw CacheException(message: e.toString());
+    }   
   }
-
 }
