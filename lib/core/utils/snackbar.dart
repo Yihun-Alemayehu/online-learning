512a04712a61a0fa7b7dcf8b9ba2ff174634b398
Yihun <yankure01@gmail.com>
2024-03-05 12:42:15 +0300

    sign_in_v.1

---
+++ lib/core/utils/snackbar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:online_learning/core/res/colors.dart';
+
+class MySnackBar {
+  static void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(
+          content: Text(
+            message,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colours.primaryColour,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: const EdgeInsets.all(10),
+        ),
+      );
+  }
+}
--- lib/features/auth/presentation/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_learning/core/common/app/provider/user_provider.dart';
+import 'package:online_learning/core/common/widgets/gradient_background.dart';
+import 'package:online_learning/core/res/fonts.dart';
+import 'package:online_learning/core/res/media_files.dart';
+import 'package:online_learning/core/utils/snackbar.dart';
+import 'package:online_learning/features/auth/presentation/bloc/auth_bloc.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -10,23 +17,42 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Sign In Screen'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context,'/');
-              },
-              child: const Text('Go to OnBoarding Screen'),
+        body: BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthErrorState) {
+          MySnackBar.showSnackBar(context, state.errorMessage);
+        } else if (state is SignedInState) {
+          context.read<UserProvider>().initUser(state.user);
+          Navigator.pushReplacementNamed(context, '/dashboard');
+        }
+      },
+      builder: (context, state) {
+        return const GradientBackground(
+          image: MediaFiles.onBoardingBackground,
+          child: SafeArea(child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Text('Easy to learn, Discover more skills',
+                  style: TextStyle(
+                    fontFamily: Fonts.aeonik,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 30,
+                  ),)
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          )),
+        );
+      },
+    ));
   }
 }
